a2e6f4895ee8ed9151ceee99e42555e5de70a044
axel <axel78944@gmail.com>
2025-05-25 22:29:03 -0400

    aaa

--- Desktop/programacion/proyecto/src/proyecto/Proyecto.java
+++ Desktop/programacion/proyecto/src/proyecto/Proyecto.java
@@ -6,7 +6,7 @@ public class Proyecto {
     public static void main(String[] args) {
         System.out.println("hola");
         //hollaaaqaa\
-        //POor la chucha
+        //POor la chucha699999
         }
 }
 
