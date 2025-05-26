7fea72e8fc5838bda625dc5a85bd0a074579c176
Jose <josemiguelpb2004@gmail.com>
2025-05-25 21:13:48 -0400

    XDDDDDDDDDDDDDDDDDDDDDD

--- Desktop/programacion/proyecto/src/proyecto/Proyecto.java
+++ Desktop/programacion/proyecto/src/proyecto/Proyecto.java
@@ -11,7 +11,10 @@ package proyecto;
 public class Proyecto {
 
     public static void main(String[] args) {
+
         System.out.println("hola");
+        System.out.println("cHULApi");
+
     }
     
 }
