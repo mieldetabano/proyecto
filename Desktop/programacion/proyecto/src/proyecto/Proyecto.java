71f65c3774ce9342158e1737dbacc866d1270ec5
Jose <josemiguelpb2004@gmail.com>
2025-05-25 22:17:09 -0400

    Hola hice un cambio

--- Desktop/programacion/proyecto/src/proyecto/Proyecto.java
+++ Desktop/programacion/proyecto/src/proyecto/Proyecto.java
@@ -1,17 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package proyecto;
 
-/**
- *
- * @author axel7
- */
 public class Proyecto {
 
     public static void main(String[] args) {
-        //holaohla
         System.out.println("hola");
-    }//me cai222333qdqw
+        }
 }
+
