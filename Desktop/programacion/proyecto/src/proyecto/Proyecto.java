15e012787fdd0af0c92d7f552cd213ffa8fc764b
axel7 <axel7@DESKTOP-F2R05DH>
2025-05-25 21:18:37 -0400

    poto

--- Desktop/programacion/proyecto/src/proyecto/Proyecto.java
+++ Desktop/programacion/proyecto/src/proyecto/Proyecto.java
@@ -1,17 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyecto;
-
-/**
- *
- * @author axel7
- */
 public class Proyecto {
 
     public static void main(String[] args) {
         System.out.println("hola");
+        System.out.println("hola peruano culiao");
     }
-    
 }
