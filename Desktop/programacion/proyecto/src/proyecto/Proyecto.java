1863634817498b6ccfdfae06f37756f3ed05b1c9
ola <msantelices226@gmail.com>
2025-05-25 21:59:12 -0400

    maraca

--- Desktop/programacion/proyecto/src/proyecto/Proyecto.java
+++ Desktop/programacion/proyecto/src/proyecto/Proyecto.java
@@ -13,6 +13,6 @@ public class Proyecto {
     public static void main(String[] args) {
         //holaohla
         System.out.println("hola");
-    }//me cai pipipipi
+    }//me cai pepepe
     
 }
