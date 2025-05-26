4185dc18fd93543af86f3aa99ea277fc911d92df
Jose <josemiguelpb2004@gmail.com>
2025-05-25 22:36:23 -0400

    hice una suma

--- Desktop/programacion/proyecto/src/proyecto/Proyecto.java
+++ Desktop/programacion/proyecto/src/proyecto/Proyecto.java
@@ -1,12 +1,20 @@
 
 package proyecto;
 
+import java.util.Scanner;
+
 public class Proyecto {
 
     public static void main(String[] args) {
         System.out.println("hola");
         //hollaaaqaa\
         //POor la chucha699999
+        //jJajajajajajajaj
+        int num = 1;
+        int num2 = 2;
+        int resultado = num + num2;
+        System.out.println(resultado);
+
         }
 }
 
