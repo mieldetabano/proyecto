78243fca9db4ce2ea6566cc88abe27c3c3f66116
Jose <josemiguelpb2004@gmail.com>
2025-05-25 21:30:31 -0400

    ayuda quedo la caga con mi wea

--- Desktop/programacion/proyecto/src/proyecto/Proyecto.java
+++ Desktop/programacion/proyecto/src/proyecto/Proyecto.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package proyecto;
-import java.util.Scanner;
 
-/**
- *
- * @author axel7
- */
+package proyecto;
+
 public class Proyecto {
 
     public static void main(String[] args) {
